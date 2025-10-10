7ed9278bf5a65c342046c59d99f1647cb966aac9
Tom Kralidis <tomkralidis@gmail.com>
2025-10-10 18:43:57 -0400

    add draft KPI for link channel centre id checking

--- pywcmp/wcmp2/kpi.py
+++ pywcmp/wcmp2/kpi.py
@@ -489,6 +489,39 @@ class WMOCoreMetadataProfileKeyPerformanceIndicators:
 
         return id_, title, total, score, comments
 
+    def kpi_link_channel_centre_id(self) -> tuple:
+        """
+        Implements KPI for consistent centre identifer in link channel
+
+        :returns: `tuple` of KPI id, title, achieved score, total score,
+                  and comments
+        """
+
+        total = 0
+        score = 0
+        comments = []
+
+        id_ = gen_test_id('link_channel_centre_id')
+        title = 'Consistent link channel centre identifier'
+
+        LOGGER.info(f'Running {title}')
+
+        id_centre_id = self.data['id'].split(':')[3]
+
+        LOGGER.debug(f'WCMP2 identifier: {id_centre_id}')
+
+        for link in self.data['links']:
+            if link.get('channel', '').startswith(('origin/a/wis2', 'cache/a/wis2')):  # noqa
+                total += 1
+                link_centre_id = link['channel'].split('/')[3]
+                if link_centre_id == id_centre_id:
+                    score += 1
+                else:
+                    msg = f'centre identifiers do not match: {id_centre_id}, {link_centre_id}'  # noqa
+                    comments.append(msg)
+
+        return id_, title, total, score, comments
+
     def evaluate(self, kpi: str = None) -> dict:
         """
         Convenience function to run all tests
